1039a46278e1c8134128f0582771fcf8586b3c60
RhaegarTargaryenn <110560249+RhaegarTargaryenn@users.noreply.github.com>
2023-12-13 10:49:36 +0530

    Update HomePage.jsx

--- src/Components/HomePage.jsx
+++ src/Components/HomePage.jsx
@@ -1,13 +1,22 @@
-import React, { useState } from 'react';
+import React, { useState , useEffect} from 'react';
 import { BsSearch } from 'react-icons/bs';
 import { useNavigate } from 'react-router-dom';
 import Spinner from "../Components/Spinner";
+import AOS from "aos";
+import "aos/dist/aos.css";
 
 const HomePage = () => {
   const navigate = useNavigate();
   const [searchTerm, setSearchTerm] = useState('');
   const [searchResults, setSearchResults] = useState([]);
   const [loading, setLoading] = useState(false);
+
+
+  useEffect(() => {
+    AOS.init();
+    AOS.refresh();
+  }, []);
+
 
   const handleSearch = async () => {
     try {
@@ -38,12 +47,12 @@ const HomePage = () => {
 
   return (
     <div className="w-[100%] h-[100vh] bg-black overflow-x-hidden">
-      <div className="text-white text-center p-3 bg-red-600 shadow-yellow-500 shadow-md">
+      <div className="text-white text-center p-3 bg-red-600 shadow-yellow-500 shadow-md" data-aos="flip-down">
         <h1 className="md:text-[30px] text-[20px] font-bold tracking-[15px]">Hacker News</h1>
       </div>
 
       <div className="w-[100%]  pt-[6%] pb-6 flex mx-[10%]">
-        <div className="md:w-[60%] w-[40%]">
+        <div className="md:w-[60%] w-[40%]" data-aos="fade-up">
           <input
             className="w-[100%] h-[60px] rounded-md text-sm pl-[4%] shadow-md shadow-yellow-400"
             type="text"
@@ -53,7 +62,7 @@ const HomePage = () => {
           />
         </div>
         <button 
-          className="md:w-[140px] w-[100px] bg-white tracking-wide text-gray-800 font-thin rounded border-b-2 border-red-500 hover:border-red-600 hover:bg-red-500 hover:text-white shadow-md md:py-3 py-2 md:px-6 px-3 inline-flex items-center ml-10"
+          className="md:w-[140px] w-[100px] bg-white tracking-wide text-gray-800 font-thin rounded border-b-2 border-red-500 hover:border-red-600 hover:bg-red-500 hover:text-white shadow-md md:py-3 py-2 md:px-6 px-3 inline-flex items-center ml-10" data-aos="fade-up"
           onClick={handleSearch}
         > <span className="mx-auto ">Search</span>
           <BsSearch />
@@ -62,14 +71,14 @@ const HomePage = () => {
 
       <div className="w-full pt-7">
         <div>
-          <h2 className="text-center text-white pb-5 pt-5 tracking-widest md:text-[30px] text-[20px]">Searched Results</h2>
-          <div className='w-[70%] mx-auto bg-red-500 h-[1px]'></div>
+          <h2 className="text-center text-white pb-5 pt-5 tracking-widest md:text-[30px] text-[20px]" data-aos="fade-up">Searched Results</h2>
+          <div className='w-[70%] mx-auto bg-red-500 h-[1px]' data-aos="zoom-in"></div>
           {loading ? (
             <div className='flex justify-center pt-10'>
             <Spinner />
             </div>
           ) : (
-            <ul className='list-inside list-decimal text-xs md:text-sm'>
+            <ul className='list-inside list-decimal text-xs md:text-sm' data-aos="zoom-in">
               {searchResults.map((result) => (
                 <li
                   key={result.objectID}
